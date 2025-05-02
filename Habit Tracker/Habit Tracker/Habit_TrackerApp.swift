5128d9a4b281a3c0b06de4486e91fe4bb0263200
Phearith22 <pichsophearith.lay-1@student.uts.edu.au>
2025-05-02 18:33:12 +1000

    completed the set up and Xcode project.

---
+++ Habit Tracker/Habit Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit Tracker
+//
+//  Created by Pichsophearith lay on 2/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
