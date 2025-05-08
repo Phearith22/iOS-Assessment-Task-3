556033bcca40144d9079b338b4e64b7ffcdb3d27
Phearith22 <pichsophearith.lay-1@student.uts.edu.au>
2025-05-09 05:45:20 +1000

    add new ProgressView page.

---
+++ Habit Tracker/Habit Tracker/ProgressView.swift
@@ -0,0 +1,15 @@
+//
+//  ProgressView.swift
+//  Habit Tracker
+//
+//  Created by Pichsophearith lay on 9/5/2025.
+//
+
+import SwiftUI
+
+struct ProgressView: View {
+    var body: some View {
+            Text("Progress View page placeholder")
+                .padding()
+        }
+}
