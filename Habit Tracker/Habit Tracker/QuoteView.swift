861e478cecd4b4c70eaac812d1143131e3154903
caixinyi2004 <xinyi.cai-2@student.uts.edu.au>
2025-05-09 03:11:23 +1000

    Add profileView and implement the motivation quote

---
+++ Habit Tracker/Habit Tracker/QuoteView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct QuoteView: View {
+    @State private var quote: String = ""
+
+    private let quotes = [
+        "You miss 100% of the shots you dont take"
+        "Small stps every day!"
+        "Youre doing great!"
+        "Stay consistent!"
+        "Quokka believes in you!"
+        "Progress, not perfection!"
+    ]
+
+}
