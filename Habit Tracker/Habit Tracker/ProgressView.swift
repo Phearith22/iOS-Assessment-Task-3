fe8be2743324f7d2513771651938642faa6ef0bd
Wilson Wong <arkeanwong@gmail.com>
2025-05-09 19:22:12 +1000

    added to progress view

--- Habit Tracker/Habit Tracker/ProgressView.swift
+++ Habit Tracker/Habit Tracker/ProgressView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct ProgressView: View {
+
+    @State private var color: Color = .blue
+    @State private var date = Date.now
+    let daysOfWeek = ["M", "T", "W", "T", "F", "S", "S"]
     var body: some View {
-            Text("Progress View page placeholder")
-                .padding()
+        NavigationView{
+            ZStack{
+                Color(.white.withAlphaComponent(0.2))
+                    .ignoresSafeArea()
+                VStack {
+                    
+                    
+                    Text("Your Progress")
+                        .font(.title)
+                        .padding()
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    ScrollView{
+                        VStack(spacing: 16){
+                            //calander component
+                            
+                            //statsSection
+                            
+                            Spacer()
+                        }
+                    }
+                    //tabbat
+                }
+            }
         }
 }
---
+++ Habit Tracker/Habit Tracker/ProgressViewModel.swift
@@ -0,0 +1,100 @@
+
+
+//  ProgressViewModel.swift
+//  Habit Tracker
+//
+//  Created by Wong Wilson on 9/5/2025.
+//
+import Foundation
+import SwiftUI
+import Combine
+
+class YourProgressViewModel : ObservableObject{
+    @Published var selectedMonth: Int
+    @Published var selectedYear: Int
+    @Published var currentDate = Date()
+    @Published var daysInMonth: [Date] = []
+    @Published var completedDays: Set<String> = []
+    
+    //stats
+    @Published var currentStreak: Int = 5
+    @Published var longestStreak: Int = 10
+    @Published var completionRate: Double = 0.7
+    @Published var monthlyPoints: Int = 2400
+    
+    private var HabitViewModel : HabitViewModel
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(habitViewModel: HabitViewModel){
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.month, .year], from: Date())
+        
+        self.selectedMonth = components.month ?? 1
+        self.selectedYear = components.year ?? 2025
+        self.HabitViewModel = habitViewModel
+        // Initial setup
+        // self.generateDaysForSelectedMonth()
+        //self.loadCompletedDays()
+        habitViewModel.$habits
+            .sink { [weak self] habits in
+                self?.loadCompletedDays()
+                self?.calculateStats()
+            }
+        
+            .store(in: &cancellables)
+    }
+    
+    func generateDaysForSelectedMonth() {
+        daysInMonth.removeAll()
+        
+        var dateComponents = DateComponents()
+        dateComponents.month = selectedMonth
+        dateComponents.year = selectedYear
+        dateComponents.day = 1
+        
+        let calendar = Calendar.current
+        
+        //get the first day of the month
+        guard let firstDayOfMonth = calendar.date(from: dateComponents) else { return }
+        
+        // get the range of days in the month
+        guard let range = calendar.range(of: .day, in: .month, for: firstDayOfMonth) else {return}
+        
+        // generate all days in the mont
+        for day in range {
+                    dateComponents.day = day
+                    if let date = calendar.date(from: dateComponents) {
+                        daysInMonth.append(date)
+                    }
+                }
+        }
+        
+    func previousMonth(){
+        if selectedMonth == 1 {
+            selectedMonth = 12
+            selectedYear -= 1
+        }else {
+            selectedMonth -= 1
+        }
+        generateDaysForSelectedMonth()
+        loadCompletedDays()
+        calculateStats()
+        
+    }
+    
+    func nextMonth(){
+        if selectedMonth == 12 {
+            selectedMonth = 1
+            selectedYear += 1
+        }else {
+            selectedMonth += 1
+        }
+        generateDaysForSelectedMonth()
+        loadCompletedDays()
+        calculateStats()
+    }
+    
+    
+    
+}
+
